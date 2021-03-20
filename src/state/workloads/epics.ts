5511a59e75ca8528a526fd6acc2bb237cedbfead
nabil.nalakath <nabilnalakath.git@gmail.com>
2021-03-21 05:13:43 +0530

    1.Fixed frame height 2. Added create action

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,25 +1,32 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, map, tap, ignoreElements } from 'rxjs/operators';
+import { filter, mergeMap, map} from 'rxjs/operators';
 import { isActionOf } from 'typesafe-actions';
 
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
 
+import { WorkloadService } from './services'
 
+const workloadService = new WorkloadService
 type AppEpic = Epic<RootAction, RootAction, RootState>;
 
 const logWorkloadSubmissions: AppEpic = (action$, state$) => (
   action$.pipe(
     filter(isActionOf(workloadsActions.submit)),
-    map(action => action.payload),
-    tap((payload) => console.log('Workload submitted', payload)),
-    ignoreElements(),
+    map((action) => action.payload),
+    mergeMap(({ complexity }) =>
+      workloadService
+        .create({ complexity })
+        .then((workload) => {
+          return workloadsActions.created(workload);
+        })
+    ),
   )
 );
 
 
 export const epics = combineEpics(
-  logWorkloadSubmissions,
+   logWorkloadSubmissions,
 );
 
 export default epics;
